7422adcb33aa9580f6f1e125a73855e41fa9c4c8
Ian Philip Eglin <philip@eglin.no>
2022-09-19 12:33:57 +0200

    7 only plot second channel if the data exists in csv (#8)
    * Added check to only plot channel 1 if the CSV is missing the data column for Ch2
    
    * Checking if datafiles acctually contain needed data columns
    
    * Added shorthand for filedialog

--- plots/spectrum.py
+++ plots/spectrum.py
@@ -2,12 +2,25 @@ from handlers.confighandler import get_global_config
 import matplotlib.pyplot as plt
 
 def spectrum(data, ctx, save_callback) -> None:
-  frequency = [p[0] for p in data]
-  ref_voltage = [p[1] for p in data]
+  if (len(data) != 5):
+    return print('ERROR: Missing data columns in CSV file.\nAs of now, WaveFormsTk only supports fully detailed datafiles for spectrum (5 Columns with frequency, channels and their phases).')
+    
+  try:
+    frequency = [p[0] for p in data]
+    ref_voltage = [p[1] for p in data]
+  except IndexError:
+    return print('ERROR: Insufficient data in CSV file for Spectrum plot')
+
+  has_voltage_change = True
+
+  try:
+    voltage_change = [p[3] for p in data]
+  except IndexError:
+    has_voltage_change = False
+
   plt.plot(frequency, ref_voltage, '-', color='orange', alpha=0.85)
 
-  if (ctx['includeVoltageChange']):
-    voltage_change = [p[3] for p in data]
+  if (ctx['includeVoltageChange'] and has_voltage_change):
     plt.plot(frequency, voltage_change, '-', color='blue')
 
   plt.xlabel(f'{ctx["labels"]["x"]["text"]} [{ctx["labels"]["x"]["unit"]}]')
